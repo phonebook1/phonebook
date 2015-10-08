beb7aa42257efa567224a0180e47e1ea4837f315
Your Name <email@example.com>
2015-10-08 20:31:23 +0800

    实现根据分组获得联系人功能

--- src/main/java/cn/thiki/phonebook/persistence/PersonsRepository.java
+++ src/main/java/cn/thiki/phonebook/persistence/PersonsRepository.java
@@ -15,8 +15,8 @@ public class PersonsRepository {
      * 建立获取数据的方法
      */
     public List<Map<String, Object>> loadPersons() {
-        String sql = "select name,phoneNumber,groupId,groupName from persons join groups on pe" +
-                "rsons.groupId=groups.id ORDER BY persons.id DESC;";
+        String sql = "select persons.id,name,phoneNumber,groupName from persons join groups on persons." +
+                "groupId = groups.id";
         return DBUtil.getListMapBySQL(sql);
     }
 
